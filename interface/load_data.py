b1228c165c7fbecea71214919042d5032db1d38e
Titouan Marcone <titouan.marcone@gmail.com>
2025-12-02 10:57:18 +0100

    deuxieme essaie cette fois cest la bonne

--- interface/load_data.py
+++ interface/load_data.py
@@ -1,18 +1,20 @@
 import os
 import pandas as pd
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def load_data_fault_free_test():
-    path = os.path.join('raw_data/TEP_FaultFree_Testing.csv')
+    path = os.path.join(BASE_DIR, '..', 'raw_data', 'TEP_FaultFree_Testing.csv')
     return pd.read_csv(path)
 
 def load_data_fault_free_train():
-    path = os.path.join('raw_data/TEP_FaultFree_Training.csv')
+    path = os.path.join(BASE_DIR, '..', 'raw_data', 'TEP_FaultFree_Training.csv')
     return pd.read_csv(path)
 
 def load_data_faulty_test():
-    path = os.path.join('raw_data/TEP_Faulty_Testing.csv')
+    path = os.path.join(BASE_DIR, '..', 'raw_data', 'TEP_Faulty_Testing.csv')
     return pd.read_csv(path)
 
 def load_data_faulty_train():
-    path = os.path.join('raw_data/TEP_Faulty_Trainig.csv')
+    path = os.path.join(BASE_DIR, '..', 'raw_data', 'TEP_Faulty_Training.csv')
     return pd.read_csv(path)
